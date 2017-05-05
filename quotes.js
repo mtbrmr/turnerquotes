50bbc86bb3d04039167282089cd58a2eaec46d75
mtbrmr <mtbrmr@gmail.com>
2017-05-05 12:11:43 +0100

    Initial commit with starting quotes

---
+++ quotes.js
@@ -0,0 +1,16 @@
+var quotes = [
+  "Development is about problem solving; not problem asking.",
+  "Do what you love, love what you do.",
+  "You can only achieve what you believe.",
+  "It's not what you do; it's how you're perceived",
+  "You gotta aim for the skies",
+  "You've gotta give what you've got",
+  "You've got to walk hand in hand with people to create success.",
+  "Don\'t be a follower because you'll just see an arsehole",
+  "If you want to change the world, make it happen",
+  "If you're given chips and beans every day, you're gonna eat chips and beans",
+  "We\'re not designing websites, we\'re designing emotions",
+  "Just imagine it happening and it will happen",
+  "We build houses for people; we don\'t live in them"
+
+];
